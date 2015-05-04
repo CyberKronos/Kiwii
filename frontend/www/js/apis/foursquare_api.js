67f55a999ed86d2ebe5527304686f91fa1f945d3
Tim C <timch326@gmail.com>
2015-05-04 16:35:00 -0700

    Cards view now fetches from Foursquare by making a hard coded query.

--- frontend/www/js/apis/foursquare_api.js
+++ frontend/www/js/apis/foursquare_api.js
@@ -1,23 +1,45 @@
-(function() {
-  var FoursquareApi = function($rootScope, $http) {
-    /* Private Methods */
-    var OAUTH_TOKEN = 'RGT5ZXHWBGVROTMD1ETZN1GMK0CLTNQEBYMUHEC3OY4XAQDQ';
+(function () {
+    var FoursquareApi = function ($rootScope, $http) {
+        /* Private Methods */
+        var OAUTH_TOKEN = 'RGT5ZXHWBGVROTMD1ETZN1GMK0CLTNQEBYMUHEC3OY4XAQDQ';
+        var API_VERSION = '20141020';
+        var BASE_URL_VENUE = 'https://api.foursquare.com/v2/venues/';
 
-    /* Public Interface */
-    return {
-      getRestaurantReviews: function(foursquareId) {
-        var endpointUrl = 'https://api.foursquare.com/v2/venues/' + foursquareId + '/tips?oauth_token=' + OAUTH_TOKEN + '&v=20141020';
-        
-        Parse.Cloud.run('callFoursquareApi', { url: endpointUrl })
-        .then(function(response) {
-          var tips = response.data.response.tips.items;
-          console.log(tips);
-          $rootScope.restaurantReviews = tips;
-        });
-      }
-    }
-  }
+        /* Public Interface */
+        return {
+            getRestaurantReviews: function (foursquareId) {
+                var endpointUrl = 'https://api.foursquare.com/v2/venues/' + foursquareId + '/tips?oauth_token=' + OAUTH_TOKEN + '&v=' + API_VERSION;
 
-  angular.module('app').
-    factory('FoursquareApi', FoursquareApi)
+                Parse.Cloud.run('callFoursquareApi', {url: endpointUrl})
+                    .then(function (response) {
+                        var tips = response.data.response.tips.items;
+                        console.log(tips);
+                        $rootScope.restaurantReviews = tips;
+                    });
+            },
+            exploreRestaurants: function (queryParams) {
+                var endpointUrl = BASE_URL_VENUE + '/explore?ll=43.604500004500004,-79.6437976014868&section=food&openNow=1&radius=1000&price=2&venuePhotos=1&oauth_token=' + OAUTH_TOKEN + '&v=' + API_VERSION;
+                var venues = [];
+                return Parse.Cloud.run('callFoursquareApi', {url: endpointUrl, queryParams: queryParams})
+                    .then(function (response) {
+                        var itemsResponse = response.data.response.groups[0].items;
+
+                        venues = itemsResponse.map(function(item, index) {
+                            var venue = {};
+                            var venuePhoto = item.venue.photos.groups[0].items[0];
+                            venue['id'] = index + 1;
+                            venue['title'] = item.venue.name;
+                            venue['imdbRating'] = item.venue.rating;
+                            venue['imageUrl'] = venuePhoto.prefix + '500x500' + venuePhoto.suffix;
+                            venue['primaryColor'] = '#FF0000';
+                            return venue;
+                        });
+                        return venues;
+                    });
+            }
+        }
+    };
+
+    angular.module('app').
+        factory('FoursquareApi', FoursquareApi)
 })();
--- frontend/www/js/controllers/cards_ctrl.js
+++ frontend/www/js/controllers/cards_ctrl.js
@@ -1,78 +1,80 @@
-(function() {
-  var CardsCtrl = function($scope, Store, PosterPreloader, Actions, AppConstants, ApiConstants, $state, InstagramApi, FoursquareApi) {
-    // $scope.sideMenuIsOpen = function() {
-    //   return $ionicSideMenuDelegate.isOpenLeft();
-    // }
+(function () {
+    var CardsCtrl = function ($scope, $state, InstagramApi, FoursquareApi) {
+        // $scope.sideMenuIsOpen = function() {
+        //   return $ionicSideMenuDelegate.isOpenLeft();
+        // }
 
-    $scope.dismissShow = function(show) {
-      var i = $scope.shows.indexOf(show);
-      $scope.dislikedShow(i);
-      $scope.destroyShow(i);
-    }
-
-    $scope.saveShow = function(show) {
-      var i = $scope.shows.indexOf(show);
-      $scope.likedShow(i);
-      $scope.destroyShow(i);
-    }
-
-    /* Card callbacks from swiping */
-    $scope.destroyShow = function(index) {
-      $scope.shows.splice(index, 1);
-      if ($scope.shows.length <= 3 && !$scope.fetchInProgress) {
-        $scope.fetchInProgress = true;
-        $scope.appendShows = true;
-        Actions.fetchShows();
-      }
-    }
-
-    $scope.dislikedShow = function(index) {
-      Actions.dislikeShow($scope.shows[index].id);
-    }
-
-    $scope.likedShow = function(index) {
-      Actions.likeShow($scope.shows[index].id);
-    }
-
-    $scope.partialSwipeShow = function(index) { }
-    $scope.snapBackShow = function(index) { }
-
-    /* Initialize the state */
-    Store.bindState($scope, function(action) {
-      if (action && action.actionType == AppConstants.FETCH_SHOWS) {
-        if (action.response == ApiConstants.PENDING) {
-          $scope.fetchInProgress = true;
-        } else {
-          $scope.updateShows(Store.getShows());
-          $scope.fetchInProgress = false;
+        $scope.dismissShow = function (show) {
+            var i = $scope.shows.indexOf(show);
+            $scope.dislikedShow(i);
+            $scope.destroyShow(i);
         }
-      }
-    });
 
-    // Ensures that we don't have any duplicate shows
-    $scope.updateShows = function(shows) {
-      if ($scope.appendShows) {
-        $scope.shows = _.union($scope.shows, shows);
-        $scope.shows = _.uniq($scope.shows, false, function(show) {
-          return show.id;
-        });
-      } else {
-        $scope.shows = shows;
-      }
-    }
+        $scope.saveShow = function (show) {
+            var i = $scope.shows.indexOf(show);
+            $scope.likedShow(i);
+            $scope.destroyShow(i);
+        }
 
-    $scope.restaurantDetails = function() {
-      var foursquareId = '4e5eb1e61fc724eac4c172e0';
+        /* Card callbacks from swiping */
+        $scope.destroyShow = function (index) {
+            $scope.shows.splice(index, 1);
+            //if ($scope.shows.length <= 3 && !$scope.fetchInProgress) {
+            //    $scope.fetchInProgress = true;
+            //    $scope.appendShows = true;
+            //    //Actions.fetchShows();
+            //}
+        }
 
-      InstagramApi.getLocationImages(foursquareId);
-      FoursquareApi.getRestaurantReviews(foursquareId);
+        $scope.dislikedShow = function (index) {
+            //Actions.dislikeShow($scope.shows[index].id);
+        }
 
-      $state.go('details');
+        $scope.likedShow = function (index) {
+            //Actions.likeShow($scope.shows[index].id);
+        }
+
+        /* Initialize the state */
+        //Store.bindState($scope, function (action) {
+        //    if (action && action.actionType == AppConstants.FETCH_SHOWS) {
+        //        if (action.response == ApiConstants.PENDING) {
+        //            $scope.fetchInProgress = true;
+        //        } else {
+        //            $scope.updateShows(Store.getShows());
+        //            $scope.fetchInProgress = false;
+        //        }
+        //    }
+        //});
+
+        // Ensures that we don't have any duplicate shows
+        //$scope.updateShows = function (shows) {
+        //    if ($scope.appendShows) {
+        //        $scope.shows = _.union($scope.shows, shows);
+        //        $scope.shows = _.uniq($scope.shows, false, function (show) {
+        //            return show.id;
+        //        });
+        //    } else {
+        //        $scope.shows = shows;
+        //    }
+        //}
+
+        $scope.restaurantDetails = function () {
+            var foursquareId = '4e5eb1e61fc724eac4c172e0';
+
+            InstagramApi.getLocationImages(foursquareId);
+            FoursquareApi.getRestaurantReviews(foursquareId);
+
+            $state.go('details');
+        };
+
+        FoursquareApi.exploreRestaurants().then(
+            function (response) {
+                $scope.shows = response;
+                console.log($scope.shows);
+                $scope.$digest();   // Can't figure out how to get cards display consistently without manually calling digest cycle.
+            });
     };
 
-    Actions.fetchShows();
-  };
-
-  angular.module('app').
-    controller('CardsCtrl', CardsCtrl);
+    angular.module('app').
+        controller('CardsCtrl', CardsCtrl);
 })();
