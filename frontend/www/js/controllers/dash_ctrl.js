844fbe1ac3e13a01879482bf4e7a468831c3b588
Tim C <timch326@gmail.com>
2015-05-06 12:13:39 -0700

    Setting a location (other than current location) works too now.

--- frontend/www/js/controllers/dash_ctrl.js
+++ frontend/www/js/controllers/dash_ctrl.js
@@ -1,55 +1,50 @@
-(function() {
-  var DashCtrl = function($scope, $rootScope, $ionicSideMenuDelegate, $state, $ionicHistory) {
-    
-    console.log($ionicHistory.currentView());
+(function () {
+    var DashCtrl = function ($scope, $rootScope, $ionicSideMenuDelegate, $state, $ionicHistory) {
 
-    $scope.distanceList = {
-      min:'0',
-      max:'15',
-      value:'0.5'
+        console.log($ionicHistory.currentView());
+
+        $scope.distanceList = {
+            min: '500',
+            max: '15000',
+            step: '500'
+        };
+
+        $scope.priceList = [
+            {text: "$", value: "1"},
+            {text: "$$", value: "2"},
+            {text: "$$$", value: "3"},
+            {text: "$$$$", value: "4"}
+        ];
+
+        $scope.criteria = {
+            'radius': '500',
+            'price': '1,4'
+        };
+
+        $scope.data = {};
+
+        $rootScope.searchCriteria = $scope.criteria;
+
+        $scope.searchRestaurants = function () {
+            $rootScope.cardTitle = '';
+            $rootScope.cuisineType = '';
+            if ($scope.data.location) {
+                var latlon = $scope.data.location.geometry.location;
+                $scope.criteria.ll =  latlon.j + ',' + latlon.C;
+            }
+            $state.go('cards');
+        };
+
+        $scope.topTenRestaurants = function () {
+            $state.go('topten');
+        };
+
+        $scope.selectCuisinesPopup = function () {
+            // $ionicHistory.currentTitle('Main');
+            console.log($ionicHistory.viewHistory());
+        };
     }
 
-    $scope.priceList = [
-      { text: "$", value: "1" },
-      { text: "$$", value: "2" },
-      { text: "$$$", value: "3" },
-      { text: "$$$$", value: "4" }
-    ];
-
-    // Default values
-    $scope.data = {};
-    $scope.data.radius = '500';
-
-    $scope.updateDistance = function(item) {
-      // $window.localStorage.setItem( 'distance', item.value );
-      console.log(item);
-      $rootScope.searchCriteria['radius'] = item * 1000;
-      console.log( 'Distance: ' + $rootScope.searchCriteria['radius'] );
-    };
-
-    $scope.updatePrice = function(item) {
-      // $window.localStorage.setItem( 'price', item.value );
-      console.log(item.value);
-      $rootScope.searchCriteria['price'] = item.value;
-      console.log( 'Price: ' + $rootScope.searchCriteria['price'] );
-    };
-
-    $scope.searchRestaurants = function() {
-      $rootScope.cardTitle = '';
-      $rootScope.cuisineType = '';
-      $state.go('cards');
-    };
-
-    $scope.topTenRestaurants = function() {
-      $state.go('topten');
-    };
-
-    $scope.selectCuisinesPopup = function() {
-      // $ionicHistory.currentTitle('Main');
-      console.log($ionicHistory.viewHistory());
-    };
-  }
-
-  angular.module('app').
-    controller('DashCtrl', DashCtrl);
+    angular.module('app').
+        controller('DashCtrl', DashCtrl);
 })();
