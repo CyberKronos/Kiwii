160c1205373ab0637afff92623d89e32f94136e3
chajaemin <nickhg.cha@gmail.com>
2015-05-07 00:30:45 -0700

    implemented Facebook login function

--- frontend/www/js/actions.js
+++ frontend/www/js/actions.js
@@ -52,7 +52,49 @@
         .then(
           function(userObject) {
             if (!userObject.existed()) {
-              return $cordovaFacebook.api("/me", ["public_profile"]);
+              return $cordovaFacebook.api("/me", ["public_profile"])
+              .then(function(response) {
+                userObject.set('firstname', response.first_name);
+                userObject.set('lastname', response.last_name);
+                userObject.set('email', response.email);
+                userObject.save();
+
+                var profilePicParams = {
+                  "type": "square",
+                  "height": "300",
+                  "width": "300"
+                }
+
+                return $cordovaFacebook.api("/me/picture", profilePicParams)
+                .then(function(response){
+                  userObject.set('fbPicture', response.data.url);
+                  userObject.save();
+
+                  // $state.go('tab.dash', {});
+
+                  console.log(userObject.attributes); 
+                  
+                  // Save to cache after successful login.
+                  var profileInfo = {   
+                    email: userObject.attributes.email, 
+                    fbPicture: userObject.attributes.fbPicture,
+                    firstname: userObject.attributes.firstname,
+                    lastname: userObject.attributes.lastname,
+                    username: userObject.attributes.username 
+                  };
+
+                  console.log("User signed up through Facebook!");
+
+                  // move to store.js
+                  return $localStorage.$default({
+                    profileInfo: profileInfo
+                  });
+                }, function(error) {
+                  console.log(error);
+                });
+              }, function(error) {
+                console.log(error);
+              });
             } else {
               // $state.go('tab.dash', {});
               console.log(userObject);
@@ -63,122 +105,6 @@
             console.log(error);
           }
         );
-        // .then(
-        //   function(response) {
-        //     userObject.set('firstname', response.first_name);
-        //     userObject.set('lastname', response.last_name);
-        //     userObject.set('email', response.email);
-        //     userObject.save();
-
-        //     var profilePicParams = {
-        //       "type": "square",
-        //       "height": "300",
-        //       "width": "300"
-        //     }
-
-        //     return $cordovaFacebook.api("/me/picture", profilePicParams);
-        //   }, function(error) {
-        //     console.log(error);
-        //   }
-        // )
-        // .then(
-        //   function(response) {
-        //     userObject.set('fbPicture', response.data.url);
-        //     userObject.save();
-
-        //     // $state.go('tab.dash', {});
-        //     console.log("User signed up through Facebook!");
-
-        //     console.log(userObject.attributes); 
-            
-        //     // Save to cache after successful login.
-        //     var profileInfo = {   
-        //       email: userObject.attributes.email, 
-        //       fbPicture: userObject.attributes.fbPicture,
-        //       firstname: userObject.attributes.firstname,
-        //       lastname: userObject.attributes.lastname,
-        //       username: userObject.attributes.username 
-        //     };
-
-        //     // move to store.js
-        //     return $localStorage.$default({
-        //       profileInfo: profileInfo
-        //     });
-        //   }, function(error) {
-        //     console.log(error);
-        //   }
-        // );
-            
-        // $cordovaFacebook.login(["public_profile", "email", "user_friends"])
-        // .then(function(response) {
-        //   console.log(response);
-        //   if (!response.authResponse){
-        //     console.log("Cannot find the authResponse");
-        //     return;
-        //   }
-        //   var expDate = new Date(
-        //     new Date().getTime() + response.authResponse.expiresIn * 1000
-        //     ).toISOString();
-
-        //   var authData = {
-        //     id: String(response.authResponse.userID),
-        //     access_token: response.authResponse.accessToken,
-        //     expiration_date: expDate
-        //   }
-
-        //   return Parse.FacebookUtils.logIn(authData, {
-        //     success: function(userObject) {
-        //       if (!userObject.existed()) {
-        //         $cordovaFacebook.api("/me", ["public_profile"])
-        //         .then(function(response) {
-        //           userObject.set('firstname', response.first_name);
-        //           userObject.set('lastname', response.last_name);
-        //           userObject.set('email', response.email);
-        //           userObject.save();
-        //         }, function(error) {
-        //           console.log(error);
-        //         });
-        //         var profilePicParams = {
-        //           "type": "square",
-        //           "height": "300",
-        //           "width": "300"
-        //         }
-        //         $cordovaFacebook.api("/me/picture", profilePicParams)
-        //         .then(function(response) {
-        //           userObject.set('fbPicture', response.data.url);
-        //           userObject.save();
-        //         }, function(error) {
-        //           console.log(error);
-        //         });
-
-        //         $state.go('tab.dash', {});
-        //         console.log("User signed up through Facebook!");
-        //       } else {
-        //         $state.go('tab.dash', {});
-        //         console.log("User logged in through Facebook!");
-        //       }
-
-        //       setTimeout( function() { 
-        //           console.log(userObject.attributes); 
-        //           // Save to cache after successful login.
-        //           var profileInfo = {   
-        //             email: userObject.attributes.email, 
-        //             fbPicture: userObject.attributes.fbPicture,
-        //             firstname: userObject.attributes.firstname,
-        //             lastname: userObject.attributes.lastname,
-        //             username: userObject.attributes.username 
-        //           };
-        //           $localstorage.setObject('profileInfo', profileInfo);
-        //         }, 2000);
-        //     },
-        //     error: function(userObject, error) {
-        //       console.log("User cancelled the Facebook login or did not fully authorize.");
-        //     }
-        //   });
-        // }, function (error) {
-        //   // error
-        //   console.log(error);
-        // });
       },
 
       logout: function() {
