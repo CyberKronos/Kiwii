5f0f7b808e295b390c12e1695023eb5656b44157
Tim C <timch326@gmail.com>
2015-05-08 15:44:39 -0700

    Basic implementation of the favourites button. No persistence of the preference yet.

--- frontend/www/js/controllers/details_ctrl.js
+++ frontend/www/js/controllers/details_ctrl.js
@@ -1,10 +1,48 @@
 (function() {
-  var DetailsCtrl = function($scope, $ionicSlideBoxDelegate) {
-    
-    $scope.navSlide = function(index) {
-      $ionicSlideBoxDelegate.slide(index, 500);
-    }
-  
+  var DetailsCtrl = function($scope, $state, $timeout, $ionicPopover) {
+
+    $scope.toggleFavourite = function($event) {
+      // TODO: Update server
+      $scope.isFavourite = !$scope.isFavourite;
+      if ($scope.isFavourite) {
+        //$scope.showFavouritesPopup(); // Alternate way of showing saved popup
+        $scope.popover.show($event);
+      }
+    };
+
+    $scope.restuarant = {
+      title: 'Le Marché St. George'
+    };
+
+    //$scope.showFavouritesPopup = function() {
+    //  var favouritesPopup = $ionicPopup.show({
+    //    title: 'Added <b>Le Marché St. George</b> to your \'Saved for later\' list.',
+    //    scope: $scope,
+    //    buttons: [
+    //      {text : 'Check out your list',
+    //       onTap : function(e) {
+    //        console.log(e);
+    //         $state.go('profile');
+    //       }}
+    //    ]
+    //  });
+    //  $timeout(function() {
+    //    favouritesPopup.close();
+    //  }, 3000);
+    //};
+
+    $scope.popover = $ionicPopover.fromTemplateUrl('templates/favourites_popup.html' , {
+      scope: $scope,
+    }).then(function(popover) {
+      $scope.popover = popover;
+    });
+
+//Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.popover.remove();
+    });
+
+    $scope.isFavourite = false;
   };
 
   angular.module('app').
