18a4eb66882e12c67d099f65983418fe3098899d
Tim C <timch326@gmail.com>
2015-05-14 22:52:14 -0700

    Fixed distance slider not being in the right place initially.

--- frontend/www/js/controllers/dash_ctrl.js
+++ frontend/www/js/controllers/dash_ctrl.js
@@ -3,32 +3,27 @@
 
         console.log($ionicHistory.currentView());
 
+        // TODO: Use constant service
         var DISTANCE_LABELS = [
-            {text: 'Off the Beaten Path', minDistance: 15000, icon: "ion-android-compass"},
-            {text: 'Driving Distance', minDistance: 8000, icon: "ion-android-car"},
-            {text: 'Biking Distance', minDistance: 4000, icon: "ion-android-bicycle"},
-            {text: 'Walking Distance', minDistance: 2000, icon: "ion-android-walk"},
-            {text: 'Within a Few Blocks', minDistance: 0, icon: "ion-android-pin"},
+            {text: 'Off the Beaten Path', minDistance: 15000, icon: 'ion-android-compass'},
+            {text: 'Driving Distance', minDistance: 8000, icon: 'ion-android-car'},
+            {text: 'Biking Distance', minDistance: 4000, icon: 'ion-android-bicycle'},
+            {text: 'Walking Distance', minDistance: 2000, icon: 'ion-android-walk'},
+            {text: 'Within a Few Blocks', minDistance: 0, icon: 'ion-android-pin'}
         ];
 
         $scope.distanceLabel = DISTANCE_LABELS[3];
 
-        $scope.distanceListConfig = {
-            min: '1000',
-            max: '15000',
-            step: '1000'
-        };
-
         $scope.priceList = [
-            {text: "$", value: "1"},
-            {text: "$$", value: "2"},
-            {text: "$$$", value: "3"},
-            {text: "$$$$", value: "4"}
+            {text: '$', value: '1'},
+            {text: '$$', value: '2'},
+            {text: '$$$', value: '3'},
+            {text: '$$$$', value: '4'}
         ];
 
         $scope.criteria = {
-            'radius': '2000',
-            'price': '1,4'
+            radius: 2000,
+            price: '1,4'
         };
 
         $scope.data = {};
