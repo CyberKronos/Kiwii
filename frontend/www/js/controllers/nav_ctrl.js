fefe5255f996988f5697d4f47790277f37277707
Tim C <timch326@gmail.com>
2015-05-15 17:16:52 -0700

    Made a layout of saved preferences with static data.

--- frontend/www/js/controllers/nav_ctrl.js
+++ frontend/www/js/controllers/nav_ctrl.js
@@ -1,8 +1,15 @@
 (function() {
   var NavCtrl = function($scope, $state, $window, $timeout, Store, Actions, AppConstants, $ionicModal, $ionicSideMenuDelegate, $ionicPopover, $ionicHistory) {
 
+    $scope.favouritesList = [
+      {title: 'Vij\'s', rating: 8, category: 'Indian', image: 'https://irs0.4sqi.net/img/general/width960/rSyrlfuOIneyVMzihStGP5iYGBAcDv8sqc5vpa2qPsA.jpg'},
+      {title: 'Chambar', rating: 9.5, category: 'Restaurant', image: 'https://irs0.4sqi.net/img/general/width960/34995420_-w6_v1KWUtilq5Tem-VHV-M3-PXnFxFz2GooQA5g0UU.jpg'},
+      {title: 'Faubourg Bistro', rating: 7.8, category: 'Cafe', image: 'https://irs0.4sqi.net/img/general/width960/C83zIrAX6Obwo7WN6CznE0CXkLfAFk9FhoRkq3V7OZw.jpg'},
+      {title: 'Le Marché St. George', rating: 9.8, category: 'Cafe', image: 'https://irs2.4sqi.net/img/general/width960/8vim2slvDNthYIZYfeRXLVK7Ka0elGHBblDKAwpdn6c.jpg'}
+    ];
+
     $scope.goBack = function() {
-      var stateId = $ionicHistory.currentView().stateId; 
+      var stateId = $ionicHistory.currentView().stateId;
       // Go back to start if on login or register view
       if ((stateId == 'login') || (stateId == 'register')) {
         $state.go('start');
@@ -31,97 +38,12 @@
       if ($scope.moreOptionsPopoverIsOpen) {
         $scope.closePopover();
       }
-    }
-
-    $scope.resetPreferences = function() {
-      Actions.resetPreferences();
-      Actions.fetchShows();
-      if ($scope.moreOptionsPopoverIsOpen) {
-        $scope.closePopover();
-      }
-    }
-
-    /* Side Menu Delegate Setup */
-    $ionicSideMenuDelegate.edgeDragThreshold(25);
-
-    $scope.sideMenuIsOpen = function() {
-      return $ionicSideMenuDelegate.isOpenLeft();
-    }
-
-    $scope.$watch($scope.sideMenuIsOpen, function(isOpen) {
-      if (isOpen) {
-        $scope.sideMenuWasOpen = true;
-      } else {
-        if ($scope.sideMenuWasOpen) {
-          Actions.fetchShows();
-          $scope.sideMenuWasOpen = true;
-        }
-      }
-    });
-
-    /* Liked Shows Modal */
-    $ionicModal.fromTemplateUrl('templates/liked.html', {
-      scope: $scope,
-      animation: 'fade-in',
-      hideDelay: 0
-    }).then(function(modal) {
-      $scope.likedShowsModal = modal;
-    });
-
-    $scope.openLikedShowsModal = function() {
-      Actions.fetchLikedShows();
-      $scope.likedShowsModal.show();
-      $scope.likedShowsModalIsOpen = true;
     };
 
-    $scope.closeModal = function() {
-      $scope.likedShowsModal.hide();
-      $scope.likedShowsModalIsOpen = false;
+    $scope.removeRestaurant = function(index) {
+      $scope.favouritesList.splice(index, 1);
     };
-
-    $scope.$on('$destroy', function() {
-      $scope.likedShowsModal.remove();
-      $scope.likedShowsModalIsOpen = false;
-    });
-
-    $scope.$on('modal.hidden', function() {
-      $scope.likedShowsModalIsOpen = false;
-    });
-
-    $scope.$on('modal.removed', function() {
-      $scope.likedShowsModalIsOpen = false;
-    });
-
-    /* More options popover */
-    $ionicPopover.fromTemplateUrl('templates/more_options_menu.html', {
-      scope: $scope,
-    }).then(function(popover) {
-      $scope.popover = popover;
-    });
-
-    $scope.openPopover = function($event) {
-      $scope.popover.show($event);
-      $scope.moreOptionsPopoverIsOpen = true;
-    };
-
-    $scope.closePopover = function() {
-      $scope.popover.hide();
-      $scope.moreOptionsPopoverIsOpen = false;
-    };
-
-    $scope.$on('$destroy', function() {
-      $scope.popover.remove();
-      $scope.moreOptionsPopoverIsOpen = false;
-    });
-
-    $scope.$on('popover.hidden', function() {
-      $scope.moreOptionsPopoverIsOpen = false;
-    });
-
-    $scope.$on('popover.removed', function() {
-      $scope.moreOptionsPopoverIsOpen = false;
-    });
-  }
+  };
 
   angular.module('app').
     controller('NavCtrl', NavCtrl);
