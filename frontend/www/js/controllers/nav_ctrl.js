06a2aa2db14ad9570ee185859170d69a671ff4b9
Tim C <timch326@gmail.com>
2015-06-02 08:12:00 -0700

    Simplified the goBack function in NavCtrl. Breaks the current behaviour for "going back" in the details view, but that should be fixed when we combine the 3 details state into one.

--- frontend/www/js/controllers/nav_ctrl.js
+++ frontend/www/js/controllers/nav_ctrl.js
@@ -1,42 +1,18 @@
-(function() {
-  var NavCtrl = function($scope, $state, $window, $timeout, Store, Actions, AppConstants, $ionicModal, $ionicSideMenuDelegate, $ionicPopover, $ionicHistory) {
+(function () {
+  var NavCtrl = function ($scope, $state, $window, $timeout, Store, Actions, AppConstants, $ionicModal, $ionicSideMenuDelegate, $ionicPopover, $ionicHistory) {
 
-    $scope.goBack = function() {
-      var stateId = $ionicHistory.currentView().stateId;
-      // Go back to start if on login or register view
-      if ((stateId == 'login') || (stateId == 'register')) {
-        $state.go('start');
-      }
-      // If view is in card list, set back to dash view
-      if (stateId == 'cards' || stateId == 'profile') { 
-        if ($ionicHistory.backView().stateId == 'details') {
-          $state.go('details');
-        } else {
-          $state.go('dash');
-        }
-      }
-      // If view is in map, go back to details
-      if (stateId == 'maps') {
-        $state.go('details');
-      }
-      // If view is in any of the tabbed pages, set back to card list
-      if ((stateId == 'details') || (stateId == 'instagram') || (stateId == 'reviews')) {
-        if ($ionicHistory.backView().stateId == 'profile') {
-          $state.go('profile');
-        } else {
-          $state.go('cards');
-        }
-      }
+    $scope.goBack = function () {
+      $ionicHistory.goBack();
     };
 
-    $scope.logOut = function() {
+    $scope.logOut = function () {
       Actions.logout()
-      .then(function() {
-        $state.go('start');
-      });
+        .then(function () {
+          $state.go('start');
+        });
     };
 
-    $scope.resetPreferences = function() {
+    $scope.resetPreferences = function () {
       Actions.resetPreferences();
       Actions.fetchShows();
       if ($scope.moreOptionsPopoverIsOpen) {
@@ -47,11 +23,11 @@
     /* Side Menu Delegate Setup */
     $ionicSideMenuDelegate.edgeDragThreshold(25);
 
-    $scope.sideMenuIsOpen = function() {
+    $scope.sideMenuIsOpen = function () {
       return $ionicSideMenuDelegate.isOpenLeft();
     };
 
-    $scope.$watch($scope.sideMenuIsOpen, function(isOpen) {
+    $scope.$watch($scope.sideMenuIsOpen, function (isOpen) {
       if (isOpen) {
         $scope.sideMenuWasOpen = true;
       } else {
@@ -67,7 +43,7 @@
       scope: $scope,
       animation: 'fade-in',
       hideDelay: 0
-    }).then(function(modal) {
+    }).then(function (modal) {
       $scope.likedShowsModal = modal;
     });
   };
