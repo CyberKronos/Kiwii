2bd221622dd92eacc21c65987c0504bdb2aea642
CyberKronos <alex44nguyen@gmail.com>
2015-06-03 16:36:42 -0700

    removed unused auth code

--- frontend/www/js/controllers/start_ctrl.js
+++ frontend/www/js/controllers/start_ctrl.js
@@ -1,20 +1,5 @@
 (function() {
-  var StartCtrl = function($scope, $rootScope, $state, auth, Store, Actions, AppConstants) {
-    // auth.signin({
-    //   authParams: {
-    //     scope: 'openid offline_access',
-    //     device: 'Mobile'
-    //   },
-    //   standalone: true
-    // }, Actions.logIn, handleLoginError);
-
-    // Store.bindState($scope, function(action) {
-    //   if (auth.isAuthenticated && action && action.actionType == AppConstants.SET_CURRENT_USER) {
-    //     $rootScope.currentUser = Store.getCurrentUser();
-    //     $state.go('dash');
-    //   }
-    // });
-
+  var StartCtrl = function($scope, $rootScope, $state, Store, Actions, AppConstants) {
     function handleLoginError() {
       // Do something
     }
