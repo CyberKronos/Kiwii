7ef49c491334bc1f784ad55b8a7652beaaddd2fa
CyberKronos <alex44nguyen@gmail.com>
2015-06-04 01:15:09 -0700

    - cleaned out goBack function fix routing issues - merged info, reviews and ig into a new slide box lib

--- frontend/www/js/controllers/nav_ctrl.js
+++ frontend/www/js/controllers/nav_ctrl.js
@@ -2,31 +2,7 @@
   var NavCtrl = function($scope, $state, $window, $timeout, Store, Actions, AppConstants, $ionicModal, $ionicSideMenuDelegate, $ionicPopover, $ionicHistory) {
 
     $scope.goBack = function() {
-      var stateId = $ionicHistory.currentView().stateId;
-      // Go back to start if on login or register view
-      if ((stateId == 'login') || (stateId == 'register')) {
-        $state.go('start');
-      }
-      // If view is in card list, set back to dash view
-      if (stateId == 'cards' || stateId == 'profile') { 
-        if ($ionicHistory.backView().stateId == 'details') {
-          $state.go('details');
-        } else {
-          $state.go('dash');
-        }
-      }
-      // If view is in map, go back to details
-      if (stateId == 'maps') {
-        $state.go('details');
-      }
-      // If view is in any of the tabbed pages, set back to card list
-      if ((stateId == 'details') || (stateId == 'instagram') || (stateId == 'reviews')) {
-        if ($ionicHistory.backView().stateId == 'profile') {
-          $state.go('profile');
-        } else {
-          $state.go('cards');
-        }
-      }
+      $ionicHistory.goBack();
     };
 
     $scope.logOut = function() {
