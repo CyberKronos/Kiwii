64dcf9e7f968c379bebfb32662fc36ec05737342
CyberKronos <alex44nguyen@gmail.com>
2015-06-05 01:41:35 -0700

    added login/registration validation messages

--- frontend/www/js/controllers/login_ctrl.js
+++ frontend/www/js/controllers/login_ctrl.js
@@ -1,11 +1,24 @@
 (function() {
-  var LoginCtrl = function($scope, $q, $rootScope, $state, Actions) {
+  var LoginCtrl = function($scope, $q, $rootScope, $state, $ionicPopup, Actions) {
     $scope.loginObj = {};
     $scope.emailLogin = function() {
-        Actions.login($scope.loginObj.username, $scope.loginObj.password)
-        .then(function() {
-            $state.go('dash');
-        });
+      Actions.login($scope.loginObj.username, $scope.loginObj.password)
+      .then(
+        function() {
+          $state.go('dash');
+        }, function(error) {
+          var alertPopup = $ionicPopup.alert({
+            title: 'Login Error',
+            template: error,
+            buttons: [
+              { 
+                text: 'Ok',
+                type: 'button-assertive',
+              }
+            ]
+          });
+        }
+      );
     };
 
     $scope.facebookLogin = function() {
--- frontend/www/js/controllers/register_ctrl.js
+++ frontend/www/js/controllers/register_ctrl.js
@@ -1,11 +1,24 @@
 (function() {
-  var RegisterCtrl = function($scope, $state, $localStorage, Actions) {
+  var RegisterCtrl = function($scope, $state, $localStorage, $ionicPopup, Actions) {
     $scope.registerObj = {};
     $scope.emailRegister = function() {
         Actions.register($scope.registerObj.username, $scope.registerObj.firstname, $scope.registerObj.lastname, $scope.registerObj.email, $scope.registerObj.password)
-        .then(function() {
+        .then(
+          function() {
             $state.go('intro');
-        });
+          }, function(error) {
+            var alertPopup = $ionicPopup.alert({
+              title: 'Registration Error',
+              template: error,
+              buttons: [
+                { 
+                  text: 'Ok',
+                  type: 'button-assertive',
+                }
+              ]
+            });
+          }
+        );
     };
   }
 
