055f64b20025f94f491a2f80d3ddce0eb96e0b00
CyberKronos <alex44nguyen@gmail.com>
2015-06-15 14:45:16 -0700

    minor bug fixes

--- frontend/www/js/directives/slidebox_height_adjust.js
+++ frontend/www/js/directives/slidebox_height_adjust.js
@@ -1,17 +1,22 @@
-app.directive('dynamicHeight', function() {
-    return {
-        require: ['^ionSlideBox'],
-        link: function(scope, elem, attrs, slider) {
-            scope.$watch(function() {
-                return slider[0].__slider.selected();
-            }, function(val) {
-                //getting the height of viewport
-                var newHeight = window.getComputedStyle(elem.parents('ion-slide-box')[0], null).getPropertyValue("height");
-                if (parseInt(newHeight) > 0) {
-                    var ionScrollTag = elem.find('ion-scroll')[0];
-                    ionScrollTag.style.height = newHeight;
-                }
-            });
+(function() {
+    var dynamicHeight = function() {
+        return {
+            require: ['^ionSlideBox'],
+            link: function(scope, elem, attrs, slider) {
+                scope.$watch(function() {
+                    return slider[0].__slider.selected();
+                }, function(val) {
+                    //getting the height of viewport
+                    var newHeight = window.getComputedStyle(elem.parents('ion-slide-box')[0], null).getPropertyValue("height");
+                    if (parseInt(newHeight) > 0) {
+                        var ionScrollTag = elem.find('ion-scroll')[0];
+                        ionScrollTag.style.height = newHeight;
+                    }
+                });
+            }
         }
-    };
-});
+    }
+
+    angular.module('kiwii').
+        directive('dynamicHeight', dynamicHeight);
+})();
