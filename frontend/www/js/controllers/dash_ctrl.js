815b1dc2faa365dd8b6efc54ef7124ce4aae7f27
Tim C <timch326@gmail.com>
2015-08-26 15:01:03 -0700

    Refactored horizontal scrolling (and its hacks) into a directive - handles loading and basic display of error messages. Updated dash view to use the template.

--- frontend/www/js/controllers/dash_ctrl.js
+++ frontend/www/js/controllers/dash_ctrl.js
@@ -1,24 +1,22 @@
 (function () {
   angular.module('kiwii').
-    controller('DashCtrl', ['$scope', '$rootScope', '$timeout', '$ionicScrollDelegate',
+    controller('DashCtrl', ['$scope', '$rootScope', '$timeout', '$ionicScrollDelegate', '$ionicPopup', '$q',
       'LocationService', 'RestaurantExplorer', 'RestaurantDetails', 'AnalyticsTracking', 'CRITERIA_OPTIONS',
-      function ($scope, $rootScope, $timeout, $ionicScrollDelegate,
+      function ($scope, $rootScope, $timeout, $ionicScrollDelegate, $ionicPopup, $q,
                 LocationService, RestaurantExplorer, RestaurantDetails, AnalyticsTracking, CRITERIA_OPTIONS) {
 
-        findRestaurantsNearby();
-        findRestaurantsSavedForLater();
-        applyHorizontalScrollFix('nearby-restaurants-scroll');
-        applyHorizontalScrollFix('saved-restaurants-scroll');
+        $scope.findRestaurantsNearby = findRestaurantsNearby;
+        $scope.getSavedForLater = getSavedForLater;
 
-        $scope.doRefresh = function() {
-          findRestaurantsNearby();
-          findRestaurantsSavedForLater();
+        $scope.doRefresh = function () {
+          $scope.$broadcast('scrollList.refresh');
+
           //Stop the ion-refresher from spinning
           $scope.$broadcast('scroll.refreshComplete');
         };
 
         function findRestaurantsNearby() {
-          LocationService.fetchCurrentLocation()
+          return LocationService.fetchCurrentLocation()
             .then(function (latLng) {
               var nearbyCriteria = {
                 ll: latLng.lat + ',' + latLng.lng,
@@ -28,17 +26,7 @@
               };
               return RestaurantExplorer.findWithKiwii(nearbyCriteria);
             })
-            .then(function (results) {
-              $scope.nearbyRestaurants = results;
-            })
             .catch(showLocationError);
-        }
-
-        function findRestaurantsSavedForLater() {
-          getSavedForLater()
-            .then(function (results) {
-              $scope.savedRestaurants = results;
-            })
         }
 
         function getSavedForLater() {
@@ -48,6 +36,7 @@
             .then(_.method('toJSON'))
             .fail(function (error) {
               console.log(error);
+              return $q.reject($q);
             });
         }
 
@@ -77,63 +66,7 @@
               }
             ]
           });
-        }
-
-
-        /**
-         * Allows Horizontal Scroll Content area to be used to vertically scroll its parent's container.
-         * Needs to be used with horizontalScrollFix directive.
-         * Adapted from http://codepen.io/rajeshwarpatlolla/pen/xGWBja?editors=101
-         * Waiting for official fix in Ionic 1.1
-         * @param ionScrollHandle delegate handle of the ion-scroll content
-         */
-        function applyHorizontalScrollFix(ionScrollHandle) {
-          $timeout(function () {
-            //return false; // <--- comment this to "fix" the problem
-            var sv = $ionicScrollDelegate.$getByHandle(ionScrollHandle).getScrollView();
-
-            var container = sv.__container;
-
-            var originaltouchStart = sv.touchStart;
-            var originalmouseDown = sv.mouseDown;
-            var originaltouchMove = sv.touchMove;
-            var originalmouseMove = sv.mouseMove;
-
-            container.removeEventListener('touchstart', sv.touchStart);
-            container.removeEventListener('mousedown', sv.mouseDown);
-            document.removeEventListener('touchmove', sv.touchMove);
-            document.removeEventListener('mousemove', sv.mousemove);
-
-
-            sv.touchStart = function (e) {
-              e.preventDefault = function () {
-              };
-              originaltouchStart.apply(sv, [e]);
-            };
-
-            sv.touchMove = function (e) {
-              e.preventDefault = function () {
-              };
-              originaltouchMove.apply(sv, [e]);
-            };
-
-            sv.mouseDown = function (e) {
-              e.preventDefault = function () {
-              };
-              originalmouseDown.apply(sv, [e]);
-            };
-
-            sv.mouseMove = function (e) {
-              e.preventDefault = function () {
-              };
-              originalmouseMove.apply(sv, [e]);
-            };
-
-            container.addEventListener("touchstart", sv.touchStart, false);
-            container.addEventListener("mousedown", sv.mouseDown, false);
-            document.addEventListener("touchmove", sv.touchMove, false);
-            document.addEventListener("mousemove", sv.mouseMove, false);
-          });
+          return $q.reject(positionError);
         }
       }]);
 })();
