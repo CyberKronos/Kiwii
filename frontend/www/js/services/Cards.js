75588e72ceac1dbdd0b4cb7227a5b062e6da57c8
chajaemin <nickhg.cha@gmail.com>
2015-09-13 12:10:43 -0700

    Added Kiwii URL Links to Cards

--- frontend/www/js/services/Cards.js
+++ frontend/www/js/services/Cards.js
@@ -2,7 +2,7 @@
   var Cards = function ($q, ParseObject, FoursquareApi, UserPhotos) {
 
     var CARDS_CLASS = 'Cards';
-    var CARDS_KEYS = ['author', 'taggedRestaurant', 'photos'];
+    var CARDS_KEYS = ['author', 'taggedRestaurant', 'photos', 'externalUrl'];
 
     // Private Methods
     function createUserPhotos(cardData) {
